77ce51d6e76c22bc510c1c3de5022d36952cae4d
Fene-87 <fenekayas87@gmail.com>
2023-04-14 01:43:35 +0300

    Finish implementing associations

--- main.rb
+++ main.rb
@@ -1,10 +1,22 @@
 require './person'
 require './capitalize_decorator'
 require './trimmer_decorator'
+require './book'
+require './rental'
+require './classroom'
+
+# Test decorators
 
 person = Person.new(22, 'maximilianus')
 puts person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
+
+# Test book rentals by persons
+
+book = Book.new('Art of War', 'Sun Tzu')
+book.add_rental('2023-04-13', person)
+puts book.rentals.map(&:person).map(&:name)
+puts person.rentals.map(&:book).map(&:title)

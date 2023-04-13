5a3f041116f93597000322e987d0b04d17112723
Fene-87 <fenekayas87@gmail.com>
2023-04-13 13:23:30 +0300

    Fix linters errors

--- person.rb
+++ person.rb
@@ -1,23 +1,24 @@
 class Person
-    attr_accessor :name, :age, :parent_permission
-    attr_reader :id
-    def initialize(name = "unknown", age, parent_permission: true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  attr_accessor :name, :age, :parent_permission
+  attr_reader :id
 
-    def can_use_services?
-        of_age? || @parent_permission
-    end
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    private
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-    def of_age?
-        @age >= 18
-    end
+  private
+
+  def of_age?
+    @age >= 18
+  end
 end
 
-person1 = Person.new("Mark", 23)
+person1 = Person.new('Mark', 23)
 puts person1.name

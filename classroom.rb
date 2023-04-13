0fde6b249df0c6343a4a4a6de721fab3f941362f
Fene-87 <fenekayas87@gmail.com>
2023-04-14 01:45:53 +0300

    Fix linters errors

--- classroom.rb
+++ classroom.rb
@@ -1,13 +1,13 @@
 class Classroom
-    attr_accessor :label
-    
-    def initialize(label)
-        @label = label
-        @students = []
-    end
+  attr_accessor :label
 
-    def add_student(student)
-        @students << student
-        student.Classroom = self
-    end
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+    student.Classroom = self
+  end
 end

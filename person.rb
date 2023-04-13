def4f27edb3537a9f2f199217bdd23bb42a1aa64
Fene-87 <fenekayas87@gmail.com>
2023-04-13 12:40:51 +0300

    Add linters files

--- person.rb
+++ person.rb
@@ -1,29 +1,23 @@
 class Person
-    attr_accessor :id, :name, :age
-    def initialize(name = "unknown", age, parent_permission = true)
+    attr_accessor :name, :age, :parent_permission
+    attr_reader :id
+    def initialize(name = "unknown", age, parent_permission: true)
         @id = Random.rand(1..1000)
         @name = name
         @age = age
         @parent_permission = parent_permission
     end
 
+    def can_use_services?
+        of_age? || @parent_permission
+    end
+
     private
 
-    def of_age
-        if @age >= 18
-            return true
-        else
-            return false
-        end
-    end
-
-    public
-
-    def can_use_services
-        if of_age
-            return true
-        else
-            return false
-        end
+    def of_age?
+        @age >= 18
     end
 end
+
+person1 = Person.new("Mark", 23)
+puts person1.name
--- student.rb
+++ student.rb
@@ -1,9 +1,27 @@
-class Student < Person
-    def initialize(classroom)
-        @classroom = classroom
-    end
+require_relative 'person'
 
-    def play_hooky
-        return "¯\(ツ)/¯"
-    end
+class Student < Person
+  attr_reader :classroom, :parent_permission
+
+  def initialize(age, name = 'Unknown', classroom = 'Tenth', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
 end
+
+student1 = Student.new(23, 'Mark', 'Third')
+student2 = Student.new(17, 'Sonia', 'Eighth')
+
+# student one
+puts student1.name
+puts student1.age
+puts student1.parent_permission
+
+#  student two
+puts student2.name
+puts student2.age
+puts student2.parent_permission
